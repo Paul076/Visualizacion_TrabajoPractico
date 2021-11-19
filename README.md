d5c75c6677a6288a9d3f198ffdd4b0f6cc95ba40
Paul076 <osonocorporativo@gmail.com>
2021-11-19 08:58:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trabajopractico
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/trabajopractico)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+//onst appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
